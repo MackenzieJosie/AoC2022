7d3a441489717ba9ca7509a3833e3af870030d6a
MackenzieJosie <45947018+MackenzieJosie@users.noreply.github.com>
2022-12-01 17:38:23 -0500

    Update dec1.py

--- dec1/dec1.py
+++ dec1/dec1.py
@@ -5,12 +5,12 @@ def main():
 		for line in input_file:
 			if (len(line) > 1):
 				sum = sum + int(line)
-			else:
+			else:	
 				max.append(sum)
 				sum = 0
+		max.append(sum) # for the end of file
 	max.sort(reverse = True)
 	print(max[0])
 	print(max[0]+max[1]+max[2])
 
-#Call the main function
 main()
