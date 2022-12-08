f63fb5e02b5e8c1d7b4b8b156666ac7fbb2ab4c4
Mackenzie <mackenzie.josie@gmail.com>
2022-12-07 19:10:45 -0500

    shrunk dec6

--- dec6/dec6.py
+++ dec6/dec6.py
@@ -2,9 +2,7 @@ input = open("input.txt", "r").read()
 
 def solution(length):
     for i in range(length,len(input)):
-        combo = set([j for j in input[i-length:i]])
-        if (len(combo) == length):
-            return i         
+        if (len(set([j for j in input[i-length:i]])) == length): return i
         
 print(solution(4))
 print(solution(14))
