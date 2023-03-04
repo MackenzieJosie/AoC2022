ef1ab7968775f50772edcc595020c10d71849ceb
Mackenzie <mackenzie.josie@gmail.com>
2023-03-04 15:12:59 -0500

    cleaned up and fixed dec15

--- dec15/dec15-part2.py
+++ dec15/dec15-part2.py
@@ -1,42 +1,55 @@
-# input = open("example.txt").read().split("\n")
+# Opening file and prepping for parsing the input
 input = open("input.txt").read().replace("Sensor ","").replace(" closest beacon is ", "").replace("at ", "").replace(" ", "")
 input = input.replace("x=","").replace("y=","").replace(":",",").split("\n")
 
+# Parsing the instructions from the input
 instructions = []
 for line in input:
 	if len(line) == 0:
 		break
 	line = line.split(",")
-	# print(line)
 	instructions.append([int(line[0]),int(line[1]),int(line[2]),int(line[3])])
 
+# Simple function to return manhattan distance from one coordinate to another
 def get_manhattan_distance(x, y, x2, y2):
 	return abs(x2 - x) + abs(y2 - y)
 
+# Function to check with every instruction to see if the x, y values are in range of any beacons
 def check(x, y):
+	# loop through each instruction to check every beacon
 	for sens_x, sens_y, beac_x, beac_y in instructions:
+		# distance from beacon to sensor
 		dist = get_manhattan_distance(sens_x, sens_y, beac_x, beac_y)
+		# distance from coordinate to sensor
 		check_dist = get_manhattan_distance(sens_x, sens_y, x, y)
 
-		if (check_dist < dist):
+		# if the checked distance is smaller than the distance, that means the coord is within range of beacon
+		if (check_dist <= dist):
 			return False
 
+	# if the checked coordinate is out of range of all beacons, return true, this is the desired coordinate
 	return True
 
+# Main loop of program, loops through each instruction
 for sens_x, sens_y, beac_x, beac_y in instructions:
 	dist = get_manhattan_distance(sens_x, sens_y, beac_x, beac_y)
+	# this nested loop checks the entire border of the range from the current sensor
 	for dist_x in range(dist+1):
 		for i in range(4):
 			dist_y = dist + 1 - dist_x
+			if i % 2 == 1:
+				dist_y *= -1
+			if i > 1:
+				dist_x *= -1
+					
 			x = sens_x + dist_x
 			y = sens_y + dist_y
-			if i % 2 == 1:
-				x *= -1
-			if i > 1:
-				y *= -1
+		
+			if dist_x == 0 and i > 1:
+				x,y = y,x
 
+			# uses check function on every coordinate bordering a range until finding the solution and stopping the program
 			if 0 <= x <= 4000000 and 0 <= y <= 4000000:
 				if check(x, y):
-					# total += 1
-					print(x * 4000000 + y)
+					print(x,y,x * 4000000 + y)
 					exit()
--- dec15/dec15.py
+++ dec15/dec15.py
@@ -1,35 +1,36 @@
-# input = open("example.txt").read().split("\n")
+# Opening file and prepping for parsing the input
 input = open("input.txt").read().replace("Sensor ","").replace(" closest beacon is ", "").replace("at ", "").replace(" ", "")
 input = input.replace("x=","").replace("y=","").replace(":",",").split("\n")
 
+# Parsing the instructions from the input
 instructions = []
 for line in input:
 	if len(line) == 0:
 		break
 	line = line.split(",")
-	# print(line)
-	instructions.append([int(line[0]),int(line[1]),int(line[2]),int(line[3])])
+	instructions.append([int(line[0]),int(line[1]),int(line[2]),int(line[3])]) # each instruction is stored as [x,y,x2,y2] where x, y is the sensor and x2, y2 is the closest beacon to that sensor
 
+
+# Simple function to return manhattan distance from one coordinate to another
 def get_manhattan_distance(x, y, x2, y2):
 	return abs(x2 - x) + abs(y2 - y)
 
-area = {}
-TARGET_Y = 2000000
-
-area[TARGET_Y] = {}
+# Function for drawing out #'s where there cannot be a beacon
 def draw_area(x, y, x2, y2):
+	# man_dist is the distance from the sensor to the beacon
 	man_dist = get_manhattan_distance(x,y,x2,y2)
 	local_sum = 0
 	i = 0
+	# bottom_y is the bottom row of the diamond shape
 	bottom_y = y + man_dist
+	# this loop is to go through all the y values within the manhattan distance of the sensor, to draw the shape as shown in problem description
 	for top_y in range(y-man_dist, y):
-		# if top_y not in area and top_y == TARGET_Y:
-		# 	area[top_y] = {}
-		# if bottom_y not in area and bottom_y == TARGET_Y:
-		# 	area[bottom_y] = {}
-
+		# for this problem, the only row in the grid that matters is the TARGET_Y, so that is the only row we apply the function to
 		if top_y == TARGET_Y or bottom_y == TARGET_Y:
+			# this loops through all the x values that would be drawn
 			for new_x in range(x-i, x + i + 1):
+				# these conditions are for checking if the current y value is the target, and if the value has been drawn or not
+				# if the y value is the target, and the value is new, the local sum is incremented
 				if top_y == TARGET_Y:
 					if new_x not in area[top_y]:
 						area[top_y][new_x] = "#"
@@ -38,29 +39,35 @@ def draw_area(x, y, x2, y2):
 					if new_x not in area[bottom_y]:
 						area[bottom_y][new_x] = "#"
 						local_sum += 1
-		# 	print(new_x, end=" ")
-		# print()
-		# print(i,top_y, bottom_y)
 		bottom_y -= 1
 		i += 1
-	# if y not in area:
-	# 	area[y] = {}
+
+	# same as above except for the row of the sensor
 	if y == TARGET_Y:
 		for new_x in range(x-man_dist, x + man_dist):
 			if new_x not in area[y]:
 				area[y][new_x] = "#"
 				local_sum += 1
 	
+	# drawing where the sensor/beacon are if they are on the Target Y
 	if y == TARGET_Y:
 		area[y][x] = "S"
 	if y2 == TARGET_Y:
 		area[y2][x2] = "B"
 
+	# return the amount of #'s that were drawn from the current instruction
 	return local_sum
 
+# initializing the target y value, as well as a dict of dicts to keep track of the grid
+area = {}
+TARGET_Y = 2000000
+
+area[TARGET_Y] = {}
+
+# main loop for problem solution, adds the local sums from each instruction to the total sum
 sum = 0
 for instruction in instructions:
 	sum += draw_area(instruction[0],instruction[1],instruction[2],instruction[3])
 
-
+# print solution
 print(sum)
