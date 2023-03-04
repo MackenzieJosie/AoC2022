ef927526f989ee975e96a89fd0d03557fca80bf6
Mackenzie <mackenzie.josie@gmail.com>
2023-03-04 17:54:49 -0500

    cleaned up and fixed dec10

--- dec10/dec10-p2.py
+++ dec10/dec10-p2.py
@@ -1,3 +1,4 @@
+# Opening file parsing the input and removing last empty line
 input = open("input.txt").read().split("\n")
 input.pop()
 
@@ -5,16 +6,18 @@ cycle = 0
 X = 1
 value = 0
 
+# if cycle is divisible by 40, print new line
 def checkCycle(number):
 	if ((number) % 40) == 0:
 		print()
 
+# main loop through cycles/commands
+# prints either a lit or unlit char depending on the register X
+# if command is add, take an extra cycle
+# calls checkCycle after every print in case new line is needed
 for line in input:
 	cycle += 1
-	if value:
-		X+= value
-		value = 0
-	if cycle % 40 == X or (cycle % 40) - 1 == X or (cycle%40) - 2 == X:
+	if cycle % 40 == X or (cycle % 40) - 1 == X or (cycle % 40) - 2 == X:
 		print("█", end="")
 	else:
 		print(".", end="")
@@ -23,11 +26,10 @@ for line in input:
 		command, value = line.split(" ")
 		value = int(value)
 		cycle += 1
-		if cycle % 40 == X or (cycle % 40)+ 1 == X or (cycle%40)-1 == X:
+		if cycle % 40 == X or (cycle % 40) - 1 == X or (cycle % 40) - 2 == X:
 			print("█", end="")
 		else:
 			print(".", end="")
 		checkCycle(cycle)
-	#	X += value
+		X += value
 		
-#print(sum)
--- dec10/dec10.py
+++ dec10/dec10.py
@@ -1,29 +1,30 @@
+# Opening file parsing the input and removing last empty line
 input = open("input.txt").read().split("\n")
 input.pop()
 
 cycle = 0
 X = 1
-sum = 0
+total = 0
 value = 0
 
+# function to multiply the cycle by the value in register X on cycles 20, 60, 100, 140, 180 and 220
 def checkCycle(number):
 	if number == 20 or ((number - 20) % 40) == 0:
-		print(number, X)
 		return number * X
 	return 0
 
+# main loop through cycles/commands
+# if command is add, take an extra cycle
+# calls checkCycle every time cycle is incremented to potentially add signal strength to the total
 for line in input:
-	#sum += checkCycle(cycle)
 	cycle += 1
-	if value:
-		X+= value
-		value = 0
-	sum += checkCycle(cycle)
+	total += checkCycle(cycle)
 	if line[0] == "a":
 		command, value = line.split(" ")
 		value = int(value)
 		cycle += 1
-		sum += checkCycle(cycle)
-	#	X += value
-		
-print(sum)
+		total += checkCycle(cycle)
+		X += value
+
+# print solution		
+print(total)
