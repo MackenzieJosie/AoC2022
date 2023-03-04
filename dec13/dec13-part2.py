f3b39a776fa6e78bd0d95dec697da5dbf7c1c865
Mackenzie <mackenzie.josie@gmail.com>
2023-03-04 16:21:09 -0500

    cleaned up dec 13

--- dec13/dec13-part2.py
+++ dec13/dec13-part2.py
@@ -1,31 +1,36 @@
 import json
 from functools import cmp_to_key
 
-# input = open("example.txt").read().split("\n\n")
+# Opening file parsing the input
 input = open("input.txt").read().replace("\n\n","\n").split("\n")
 
+# recursive function to compare the left and right sides to determine if the inputs are in the right order
 def compareLists(left, right):
-	if len(left) == 0:
+	if len(left) == 0: # left list ran out of values, left is smaller
 		return -1
 	for i in range(len(left)):
-		if i >= len(right):
+		if i >= len(right): # left list is longer than right, right is smaller
 			return 1
+		# next two if statements make sure both values are lists if one is a list
 		if type(right[i]) == list and type(left[i]) != list:
 			left[i] = [left[i]]
 		if type(left[i]) == list and type(right[i]) != list:
 			right[i] = [right[i]]
-		if (type(left[i]) != list): # integer checks
+		# neither are lists, check if integer values are the same
+		if (type(left[i]) != list):
 			if right[i] < left[i]:
-				return 1
+				return 1 # right is smaller
 			elif right[i] > left[i]:
-				return -1
+				return -1 # left is smaller
 			elif i == len(left) -1:
-				return -1
+				return -1 # left ran out of values first, left is smaller
 		else:
-			return compareLists(left[i], right[i])
+			return compareLists(left[i], right[i]) # both are lists, call itself to go inside lists for further comparison
 
+	# left and right are the same
 	return 0
 
+# loads the packets from the input parsed with json.loads for ease
 packets = []
 for lines in input:
 	if len(lines) < 1:
@@ -33,14 +38,18 @@ for lines in input:
 
 	packets.append(json.loads(lines))
 
+# part 2 required these two packets be added
 packets.append([[6]])
 packets.append([[2]])
 
+# sort packets based off of compare function above
 packets = sorted(packets, key=cmp_to_key(compareLists))
 
+# find where the two decoder packets ended up
 decoder = []
 for i in range(len(packets)):
 	if packets[i] == [[[[6]]]] or packets[i] == [[[[2]]]]:
 		decoder.append(i+1)
 
-print(decoder[0]*decoder[1])
+# print solution
+print(decoder[0], " * ", decoder[1], " = ", decoder[0]*decoder[1])
--- dec13/dec13.py
+++ dec13/dec13.py
@@ -1,28 +1,32 @@
 import json
 
-# input = open("example.txt").read().split("\n\n")
+# Opening file parsing the input
 input = open("input.txt").read().split("\n\n")
 
+# recursive function to compare the left and right sides to determine if the inputs are in the right order
 def compareLists(left, right):
-	if len(left) == 0:
-		rightIndices.append(pairIndex)
-	for i in range(len(left)):
-		if i >= len(right):
+	if len(left) == 0: # left list ran out of values, left is smaller
+		return True
+	for i in range(len(left)): 
+		if i >= len(right): # left list is longer than right, right is smaller
 			return False
+		# next two if statements make sure both values are lists if one is a list
 		if type(right[i]) == list and type(left[i]) != list:
 			left[i] = [left[i]]
 		if type(left[i]) == list and type(right[i]) != list:
 			right[i] = [right[i]]
-		if (type(left[i]) != list): # integer checks
+		# neither are lists, check if integer values are the same
+		if (type(left[i]) != list):
 			if right[i] < left[i]:
-				return False
+				return False # right is smaller
 			elif right[i] > left[i]:
-				return True
+				return True # left is smaller
 			elif i == len(left) -1:
-				return True
+				return True # left list is shorter than right list, left is smaller
 		else:
-			return compareLists(left[i], right[i])
+			return compareLists(left[i], right[i]) # both are lists, call itself to go inside lists for further comparison
 
+# main loop to keep track of which and how many pairs are right
 pairIndex = 0
 rightIndices = []
 for lines in input:
@@ -30,12 +34,15 @@ for lines in input:
 		break # empty line
 
 	pairIndex += 1
+
+	# parsing the pair into left and right
 	lines = lines.split("\n")
 	left = json.loads(lines[0])
 	right = json.loads(lines[1])
-	i = 0
 	
+	# using function to compare and determine if the pair is in the right order
 	if compareLists(left,right):
 		rightIndices.append(pairIndex)
 		
+# prints solution
 print(sum(rightIndices))
