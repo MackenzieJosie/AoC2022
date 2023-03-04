4ae44dc45a9827e5eedfbbe51865e3879e765ffb
Mackenzie <mackenzie.josie@gmail.com>
2023-03-04 15:50:04 -0500

    cleaned up dec14

--- dec14/dec14-part2.py
+++ dec14/dec14-part2.py
@@ -1,16 +1,18 @@
-# input = open("example.txt").read().split("\n")
+# Opening file and prepping for parsing the input
 input = open("input.txt").read().split("\n")
 
-walls = []
+walls = [] # list for storing lists of walls
 
 max_x, min_x, max_y = 0, 500, 0
-for line in input: # grabbing instructions / parsing input
+# grabbing instructions / parsing input
+for line in input:
 	if len(line) < 1:
 		break # empty line
 
-	row = line.replace("-> ","").split(" ")
+	index = line.replace("-> ","").split(" ") # parsing into each separate coordinate in line
 	wall = []
-	for lines in row:
+	# turning each index into an (x,y) as ints, also updates mins/maxes
+	for lines in index:
 		lines = lines.split(",")
 		lines = [int(lines[0]),int(lines[1])]
 		if lines[0] > max_x: max_x = lines[0]
@@ -18,6 +20,7 @@ for line in input: # grabbing instructions / parsing input
 		if lines[0] < min_x: min_x = lines[0]
 		wall.append(lines)
 
+	# appends list containing shape of current wall to list of all walls
 	walls.append(wall)
 
 # cave grid initialization
@@ -30,7 +33,9 @@ for i in range(width):
 	cave[height-1][i] = "#"
 
 cave[source[0]][source[1]] = "+"
+# end of initialization
 
+# function for drawing the walls on grid between to points
 def draw_line(point_1, point_2):
 	cave[point_1[1]][point_1[0]] = "#"
 	while point_1 != point_2:
@@ -43,30 +48,35 @@ def draw_line(point_1, point_2):
 		if point_1[1] > point_2[1]:
 			point_1[1] -= 1
 		cave[point_1[1]][point_1[0]] = "#"
-	
+
+# loop through walls list to draw all walls parsed from input
 for wall in walls: # drawing walls
 	for i in range(len(wall)-1):
 		draw_line(wall[i], wall[i+1])
 
-
+# function for pouring the sand into the grid from the source
 def pour_sand(source):
-	unit = [source[0], source[1]]
+	unit = [source[0], source[1]] # set the unit of sands coords to the same as the source
+	# this loop is the simulation of the falling sand until no more sand can be poured
 	while cave[unit[0]][unit[1]] == "." or cave[unit[0]][unit[1]] == "+":
 		unit[0] += 1
-		if cave[unit[0]][unit[1]] != ".":
+		if cave[unit[0]][unit[1]] != ".": # found a spot that's not empty, check if can go left or right instead
 			if cave[unit[0]][unit[1]-1] == ".":
 				unit[1] -= 1
 			elif cave[unit[0]][unit[1]+1] == ".":
 				unit[1] += 1
-	if cave[unit[0]-1][unit[1]] == "+":
+	if cave[unit[0]-1][unit[1]] == "+": # if made it back to the source, no more sand can be poured, return
 		return False
-	cave[unit[0]-1][unit[1]] = "o"
+	cave[unit[0]-1][unit[1]] = "o" # update grid with new piece of sand and return for the next sand to be dropped
 	return True
 
-sand_count = 0
+# loop to pour sand until no more sand can be poured, keeping track of total amount of sand poured
+sand_count = 1
 while pour_sand(source):
 	sand_count += 1
 
+print(sand_count) # prints solution
+
+# can also use this to print the result of the grid to look at result or debug
 # for line in cave: # printing cave to check
 # 	print(line)
-print(sand_count + 1)
--- dec14/dec14.py
+++ dec14/dec14.py
@@ -1,16 +1,18 @@
-input = open("example.txt").read().split("\n")
-# input = open("input.txt").read().split("\n")
+# Opening file and prepping for parsing the input
+input = open("input.txt").read().split("\n")
 
-walls = []
+walls = [] # list for storing lists of walls
 
 max_x, min_x, max_y = 0, 500, 0
+# grabbing instructions / parsing input
 for line in input: # grabbing instructions / parsing input
 	if len(line) < 1:
 		break # empty line
 
-	row = line.replace("-> ","").split(" ")
+	index = line.replace("-> ","").split(" ") # parsing into each separate coordinate in line
 	wall = []
-	for lines in row:
+	# turning each index into an (x,y) as ints, also updates mins/maxes
+	for lines in index:
 		lines = lines.split(",")
 		lines = [int(lines[0]),int(lines[1])]
 		if lines[0] > max_x: max_x = lines[0]
@@ -18,6 +20,7 @@ for line in input: # grabbing instructions / parsing input
 		if lines[0] < min_x: min_x = lines[0]
 		wall.append(lines)
 
+	# appends list containing shape of current wall to list of all walls
 	walls.append(wall)
 
 # cave grid initialization
@@ -26,7 +29,9 @@ height = max_y + 1
 source = [0, 500-min_x]
 cave = [["."] * width for i in range(height)]
 cave[source[0]][source[1]] = "+"
+# end of grid initialilzation
 
+# function for drawing the walls on grid between to points
 def draw_line(point_1, point_2):
 	cave[point_1[1]][point_1[0]-min_x] = "#"
 	while point_1 != point_2:
@@ -40,11 +45,13 @@ def draw_line(point_1, point_2):
 			point_1[1] -= 1
 		cave[point_1[1]][point_1[0]-min_x] = "#"
 	
+# loop through walls list to draw all walls parsed from input
 for wall in walls: # drawing walls
 	for i in range(len(wall)-1):
 		draw_line(wall[i], wall[i+1])
 
 
+# function for pouring the sand into the grid from the source
 def pour_sand(source):
 	unit = [source[0] + 1, source[1]]
 	while cave[unit[0]][unit[1]] == ".":
@@ -59,10 +66,13 @@ def pour_sand(source):
 	cave[unit[0]-1][unit[1]] = "o"
 	return True
 
+# loop to pour sand until no more sand can be poured, keeping track of total amount of sand poured
 sand_count = 0
 while pour_sand(source):
 	sand_count += 1
 
+print(sand_count) # printing solution
+
+# can also use this to print the result of the grid to look at result or debug
 # for line in cave: # printing cave
 # 	print(line)
-print(sand_count)
